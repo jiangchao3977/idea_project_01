0137ab6f368b4f854d4ca536c88b0ba307a5f27d
姜超 <1527243306@qq.com>
2019-08-31 22:37:00 +0800

    把新建的模块添加到本地仓库！

---
+++ git_prictice_01/src/main/java/com/itcast/git/Test01.java
@@ -0,0 +1,12 @@
+package com.itcast.git;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("这");
+        System.out.println("是");
+        System.out.println("git");
+        System.out.println("的");
+        System.out.println("练");
+        System.out.println("习");
+    }
+}
